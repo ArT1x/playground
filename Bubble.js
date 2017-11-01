8bc11baf9f85030278f53ad8c1aee1e593a08042
ArT1x <ArT1x@users.noreply.github.com>
2017-11-01 16:02:44 +0100

    Added Sliders color(r, g, b), size. Libraries Folder back again + p5.dom.min.js.
    index.html:
    changed background-color
    added Header

--- Bubble.js
+++ Bubble.js
@@ -1,26 +1,31 @@
 class Bubble {
-  constructor(x, y, r, col, life) {
+  constructor(x, y, r, colr, colg, colb, lifetime) {
     this.x = x;
     this.y = y;
+    //radius
     this.r = r;
-    this.col = col;
-    this.life = life;
+    //color
+    this.colr = colr;
+    this.colg = colg;
+    this.colb = colb;
+
+    this.lifetime = lifetime;
   }
   update() {
     this.x = this.x + random(-5, 5);
     this.y = this.y + random(-5, 5);
   }
   show() {
-    stroke(this.col, 100, this.col, this.life);
+    stroke(this.colr, this.colg, this.colb, this.lifetime);
     strokeWeight(4);
-    noFill();
-    //fill(255);
+    //noFill();
+    fill(this.colr, this.colg, this.colb, this.lifetime);
     ellipse(this.x, this.y, this.r * 2);
     this.y = this.y + -4;
-    this.life = this.life - 7;
+    this.lifetime = this.lifetime - 7;
   }
   isFinished() {
-    if (this.life < 0) {
+    if (this.lifetime < 0) {
       return true;
     } else {
       return false;
--- sketch.js
+++ sketch.js
@@ -1,14 +1,49 @@
 let bubbles = [];
 
 function setup() {
-  createCanvas(1024, 768);
+
+  createCanvas(900, 400);
+
+  //Slider RED
+  textr = createP("red");
+  textr.style('color', 'red');
+  sliderr = createSlider(0, 255, 100, 10);
+  sliderr.style('background-color', 'red');
+
+  //Slider Green
+  textg = createP("green");
+  textg.style('color', 'green');
+  sliderg = createSlider(0, 255, 100, 10);
+  sliderg.style('background-color', 'green');
+
+  //Slider BLUE
+  textb = createP("blue");
+  textb.style('color', 'blue');
+  sliderb = createSlider(0, 255, 100, 10);
+  sliderb.style('background-color', 'blue');
+
+  //slider SIZE
+  createP("size");
+  sliders = createSlider(1, 50, 15, 5);
+
 }
 
 function mouseDragged() {
-  let r = random(5, 20);
-  let life = 200;
-  let col = random(100, 255);
-  bubbles.push(new Bubble(mouseX, mouseY, r, col, life));
+
+  //bubble size
+  let bsize = sliders.value();
+  let r = random(bsize * 0.5, bsize * 2);
+
+  let lifetime = 200;
+  //colors
+  //let colr = random(100, 255)
+  let colr = sliderr.value();
+  //let colg = random(100, 255);
+  let colg = sliderg.value();
+  //let colb = random(100, 255);
+  let colb = sliderb.value();
+
+  bubbles.push(new Bubble(mouseX, mouseY, r, colr, colg, colb, lifetime));
 }
 
 function draw() {
